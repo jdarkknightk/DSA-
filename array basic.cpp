04bdb9a0e9a0e14b9479d8e8f9f2115aa758864a
Jaivardhan Kanoria <158453391+jdarkknightk@users.noreply.github.com>
2025-08-11 05:07:28 +0530

    Add files via upload

---
+++ array basic.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+    cin >> arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    arr[2] +=10;
+    cout << arr[2];
+    return 0;
+}
+    
